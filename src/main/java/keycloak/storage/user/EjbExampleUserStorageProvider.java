7cfc2cc3f606a8aeff020ba572872f93776f7dc7
vasil <andr_vasil@mail.ru>
2017-08-17 13:04:34 +0300

    Рабочая версия

--- src/main/java/keycloak/storage/user/EjbExampleUserStorageProvider.java
+++ src/main/java/keycloak/storage/user/EjbExampleUserStorageProvider.java
@@ -59,7 +59,7 @@ public class EjbExampleUserStorageProvider implements UserStorageProvider,
     private static final Logger log = Logger.getLogger(EjbExampleUserStorageProvider.class);
     public static final String PASSWORD_CACHE_KEY = UserAdapter.class.getName() + ".password";
 
-    @PersistenceContext
+    @PersistenceContext(unitName = "user-storage-jpa-example")
     protected EntityManager em;
     protected ComponentModel model;
     protected KeycloakSession session;
