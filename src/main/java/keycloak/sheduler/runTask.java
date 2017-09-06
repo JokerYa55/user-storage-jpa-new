5566f22ff17a108ad02a6886f3db224909c1a118
vasil <andr_vasil@mail.ru>
2017-09-06 18:42:02 +0300

    Рабочая версия

---
+++ src/main/java/keycloak/sheduler/runTask.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package keycloak.sheduler;
+
+import java.util.logging.Logger;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import org.keycloak.examples.storage.HTTPUtil.Util;
+
+/**
+ *
+ * @author vasil
+ */
+@Singleton
+public class runTask {
+
+    Logger log = Logger.getLogger(runTask.class.getName());
+    long count = 0;
+    @Schedule(hour="*", minute="1")
+    public void runsEveryMinute() {
+        count ++;
+        log.info(" runs EveryMinute " + count);
+        log.info(Util.doGet("http://sql.ru", null));
+    }
+}
